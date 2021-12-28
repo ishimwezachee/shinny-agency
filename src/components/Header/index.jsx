1dadb3a3f6818d69cf74e0c9a811978aae631385
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-28 14:34:56 +0200

    configure react router

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/survey">Survey</Link>
+    </nav>
+  );
+}
+
+export default Header;
